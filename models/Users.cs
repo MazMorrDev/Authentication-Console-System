3da5901665791b29708dc4f217144cf5a8e0bee8
MazMorrDev <marconchelo12@gmail.com>
2025-11-01 21:31:39 -0400

    feat: user model

---
+++ models/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthenticationConsoleSystem;
+
+public class Users
+{
+    public int Id { get; set; }
+    public string? UserName { get; set; }
+    public string? Email { get; set; }
+    public string? Password{ get; set; }
+}
